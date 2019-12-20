bfae77912228f9605e8e73460b71b3c68d93f1db
lyx <416698547@qq.com>
2019-12-20 14:20:55 +0800

    准备修改页面，基本完成

--- src/main/java/com/lyx/controller/ContentController.java
+++ src/main/java/com/lyx/controller/ContentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -29,10 +30,28 @@ public class ContentController
 		return service.list();
 	}
 
+	/**
+	 * 添加一条内容
+	 * @param content
+	 * @return
+	 */
 	@PostMapping("/addOneContent")
 	public String addOneContent(Content content)
 	{
 		service.save(content);
+
+		return "redirect:/";
+	}
+
+	/**
+	 * 删除一条内容
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/removeOneContent")
+	public String removeOneContent(@RequestParam("id") int id)
+	{
+		service.removeById(id);
 
 		return "redirect:/";
 	}
