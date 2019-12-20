04f2df34434ed75cbd71b4a84327509b633fdea2
lyx <416698547@qq.com>
2019-12-20 11:22:20 +0800

    添加完成

--- src/main/java/com/lyx/controller/ContentController.java
+++ src/main/java/com/lyx/controller/ContentController.java
@@ -4,12 +4,14 @@ import com.lyx.entity.Content;
 import com.lyx.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ContentController
 {
 	@Autowired
@@ -17,12 +19,21 @@ public class ContentController
 	private ContentService service;
 
 	/**
-	 * 查久所有内容
+	 * 查询所有内容
 	 * @return
 	 */
 	@GetMapping("/listAllContent")
+	@ResponseBody
 	public List<Content> listAllContent()
 	{
 		return service.list();
+	}
+
+	@PostMapping("/addOneContent")
+	public String addOneContent(Content content)
+	{
+		service.save(content);
+
+		return "redirect:/";
 	}
 }
