4b834b89f0966996fe15e4ba9c32a48e74c25a10
李艳兴 <liyanxing@zhongruigroup.com>
2021-11-01 17:22:56 +0800

    复制 修改完成

--- src/main/java/com/lyx/controller/ContentController.java
+++ src/main/java/com/lyx/controller/ContentController.java
@@ -12,71 +12,98 @@ import java.util.List;
 @Controller
 public class ContentController
 {
-	@Autowired
-	@Qualifier("contentServiceImpl")
-	private ContentService service;
+    @Autowired
+    @Qualifier("contentServiceImpl")
+    private ContentService service;
 
-	/**
-	 * 添加一条内容
-	 * @param content
-	 * @return
-	 */
-	@PostMapping("/addOneContent")
-	public String addOneContent(Content content)
-	{
-		service.addOneContent(content);
+//    /**
+//     * 添加一条内容
+//     * @param content
+//     * @return
+//     */
+//    @PostMapping("/addOneContent")
+//    public String addOneContent(Content content)
+//    {
+//        service.addOneContent(content);
+//
+//        return "redirect:/";
+//    }
+//
+//    /**
+//     * 删除一条内容
+//     * @param id
+//     * @return
+//     */
+//    @GetMapping("/removeOneContent")
+//    public String removeOneContent(@RequestParam("id") int id)
+//    {
+//        service.removeOneContent(id);
+//
+//        return "redirect:/";
+//    }
+//
+//
+//
+//    @GetMapping("/content/{id}")
+//    public String changeOrder(@PathVariable("id") int id, @RequestParam boolean isUp)
+//    {
+//        service.changeOrder(id, isUp);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/content/first-or-last/{id}")
+//    public String setFirstOrLast(@PathVariable("id") int id, @RequestParam boolean isFirst)
+//    {
+//        service.setFirstOrLast(id, isFirst);
+//        return "redirect:/";
+//    }
 
-		return "redirect:/";
-	}
 
-	/**
-	 * 删除一条内容
-	 * @param id
-	 * @return
-	 */
-	@GetMapping("/removeOneContent")
-	public String removeOneContent(@RequestParam("id") int id)
-	{
-		service.removeOneContent(id);
 
-		return "redirect:/";
-	}
 
-	/**
-	 * 根据id更新一条内容
-	 * @param content
-	 * @return
-	 */
-	@PostMapping("/updateContextById")
-	public String updateContextById(Content content)
-	{
-		service.updateById(content);
 
-		return "redirect:/";
-	}
 
-	@GetMapping("/content/{id}")
-	public String changeOrder(@PathVariable("id") int id, @RequestParam boolean isUp)
-	{
-		service.changeOrder(id, isUp);
-		return "redirect:/";
-	}
 
-	@GetMapping("/content/first-or-last/{id}")
-	public String setFirstOrLast(@PathVariable("id") int id, @RequestParam boolean isFirst)
-	{
-		service.setFirstOrLast(id, isFirst);
-		return "redirect:/";
-	}
 
-	/**
-	 * 查询所有内容
-	 * @return
-	 */
-	@GetMapping("/listAllContent")
-	@ResponseBody
-	public List<Content> listAllContent()
-	{
-		return service.listAllContent();
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 根据id更新一条内容
+     * @param content
+     * @return
+     */
+    @PostMapping("/updateContextById")
+    @ResponseBody
+    public void updateContextById(@RequestBody Content content)
+    {
+        service.updateById(content);
+    }
+
+    /**
+     * 查询所有内容
+     * @return
+     */
+    @GetMapping("/listAllContent")
+    @ResponseBody
+    public List<Content> listAllContent()
+    {
+        return service.listAllContent();
+    }
 }
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -7,7 +7,7 @@ $(
 )
 
 /**
- * 页面初始化，获取数据.
+ * 页面初始化
  */
 function init()
 {
@@ -21,6 +21,8 @@ function init()
             {
                 setHtml(body);
                 editEvent();
+                backEvent();
+                submitEvent();
             },
             error : function ()
             {
@@ -40,10 +42,10 @@ function setHtml(body)
     {
         let html = '    <div class="component">' +
             '                <div>' +
-            '                    <span id="cid' + el.contentId + '" class="content col-lg-7"></span>' +
+            '                    <span id="c' + el.contentId + '" class="content col-lg-7"></span>' +
             '                    <div class="btn-group col-lg-5" role="group">' +
-            '                        <button type="button" class="btn btn-primary" data-clipboard-action="copy" data-clipboard-target="#cid' + el.contentId + '">复制</button>' +
-            '                        <button type="button" class="btn btn-success">修改</button>' +
+            '                        <button type="button" class="btn btn-primary" data-clipboard-action="copy" data-clipboard-target="#c' + el.contentId + '">复制</button>' +
+            '                        <button type="button" class="btn btn-success" my="update">修改</button>' +
             '                        <button type="button" class="btn btn-danger">删除</button>' +
             '                        <button type="button" class="btn btn-info">上移</button>' +
             '                        <button type="button" class="btn btn-info">下移</button>' +
@@ -51,16 +53,17 @@ function setHtml(body)
             '                        <button type="button" class="btn btn-info">移到最后</button>' +
             '                    </div>' +
             '                </div>' +
-            '                <div class="edit">' +
-            '                    <input type="text" class="content col-lg-7" value="sdfaf"/>' +
+            '                <div class="hide">' +
+            '                    <input type="text" class="content col-lg-7"/>' +
             '                    <div class="btn-group col-lg-5" role="group">' +
-            '                        <button type="button" class="btn btn-info">提交</button>' +
-            '                        <button type="button" class="btn btn-info">返回</button>' +
+            '                        <button type="button" class="btn btn-info" my="mSubmit">提交</button>' +
+            '                        <button type="button" class="btn btn-info" my="back">返回</button>' +
             '                    </div>' +
             '                </div>' +
             '            </div>'
         $('div.container').append(html);
         $('span.content:last').text(el.contentMaster);
+        $('input.col-lg-7:last').val(el.contentMaster);
     }
 }
 
@@ -69,12 +72,72 @@ function setHtml(body)
  */
 function editEvent()
 {
-    $('.btn-success').bind
+    $('button[my=update]').bind
     (
         'click',
         function ()
         {
-            $(this).parent().parent().hide();
+            $(this).parent().parent().addClass('hide');
+            $(this).parent().parent().next().removeClass('hide');
+        }
+    );
+}
+
+/**
+ * 返回按钮绑定事件
+ */
+function backEvent()
+{
+    $('button[my=back]').bind
+    (
+        'click',
+        function ()
+        {
+            $(this).parent().parent().addClass('hide');
+            $(this).parent().parent().prev().removeClass('hide');
+        }
+    );
+}
+
+/**
+ * 提交按钮绑定事件
+ */
+function submitEvent()
+{
+    $('button[my=mSubmit]').bind
+    (
+        'click',
+        function ()
+        {
+            let value = $(this).parent().prev().val();
+            let cid = $(this).parent().parent().prev().children(':first').attr('id');
+            var id = cid.substr(cid.indexOf('c')+1);
+            $.ajax
+            (
+                {
+                    url : '/updateContextById',
+                    type : 'POST',
+                    data:JSON.stringify
+                    (
+                        {
+                            "contentId": id,
+                            "contentMaster": value
+                        }
+                    ),
+                    headers:
+                    {
+                        'Content-Type': 'application/json'
+                    },
+                    success: function ()
+                    {
+                        window.location.reload();
+                    },
+                    error: function ()
+                    {
+                        alert('请求失败');
+                    }
+                }
+            );
         }
     );
 }
